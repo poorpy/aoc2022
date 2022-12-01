829567ce1e7469fb90442b1a58246ddc3566082e
Bartosz Marczyński <marczynski.bartosz@gmail.com>
2022-12-01 07:55:04 +0100

    day01: part1

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,3 +1,24 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let filename: &str = &args[1];
+    let calories = read_calories(filename);
+
+    println!("max calories: {}", calories.iter().max().unwrap())
+}
+
+fn read_calories(filename: &str) -> Vec<u64> {
+    let contents = fs::read_to_string(filename).expect(&format!(
+        "Something went wrong reading the file: {}",
+        filename
+    ));
+    contents
+        .split("\n\n")
+        .map(|x| {
+            x.split_whitespace()
+                .map(|s| s.parse::<u64>().unwrap())
+                .sum()
+        })
+        .collect()
 }
