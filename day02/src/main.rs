9178282a8153a655405ebb921351a8c710dc929a
Bartosz Marczyński <marczynski.bartosz@gmail.com>
2022-12-02 07:40:39 +0100

    day02: part 2

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -4,6 +4,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let filename: &str = &args[1];
     let strategy = read_strategy(filename);
+    let with_outcome = read_strategy_with_outcome(filename);
 
     println!(
         "{}",
@@ -12,6 +13,15 @@ fn main() {
             .map(|round| round_score(*round))
             .sum::<u64>()
     );
+
+    println!(
+        "{}",
+        with_outcome
+            .iter()
+            .map(|round| outcome_into_figures(*round))
+            .map(|round| round_score(round))
+            .sum::<u64>()
+    )
 }
 
 #[derive(Copy, Clone, Debug, PartialEq)]
@@ -29,6 +39,40 @@ impl From<&str> for Figure {
             "C" | "Z" => Figure::Scissors,
             _ => Figure::Rock,
         }
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+enum Outcome {
+    Lose,
+    Draw,
+    Win,
+}
+
+impl From<&str> for Outcome {
+    fn from(s: &str) -> Self {
+        match s {
+            "X" => Outcome::Lose,
+            "Y" => Outcome::Draw,
+            "Z" => Outcome::Win,
+            _ => Outcome::Lose,
+        }
+    }
+}
+
+fn outcome_into_figures(round: (Figure, Outcome)) -> (Figure, Figure) {
+    match round {
+        (x, Outcome::Draw) => (x, x),
+        (x, Outcome::Win) => match x {
+            Figure::Rock => (x, Figure::Paper),
+            Figure::Paper => (x, Figure::Scissors),
+            Figure::Scissors => (x, Figure::Rock),
+        },
+        (x, Outcome::Lose) => match x {
+            Figure::Rock => (x, Figure::Scissors),
+            Figure::Paper => (x, Figure::Rock),
+            Figure::Scissors => (x, Figure::Paper),
+        },
     }
 }
 
@@ -64,6 +108,20 @@ fn read_strategy(filename: &str) -> Vec<(Figure, Figure)> {
                 .map(|s| Figure::from(s))
                 .collect::<Vec<Figure>>();
             (row[0], row[1])
+        })
+        .collect()
+}
+fn read_strategy_with_outcome(filename: &str) -> Vec<(Figure, Outcome)> {
+    let contents = fs::read_to_string(filename).expect(&format!(
+        "Something went wrong reading the file: {}",
+        filename
+    ));
+    contents
+        .split("\n")
+        .filter(|l| !l.is_empty())
+        .map(|row| {
+            let row = row.split_whitespace().collect::<Vec<&str>>();
+            (Figure::from(row[0]), Outcome::from(row[1]))
         })
         .collect()
 }
