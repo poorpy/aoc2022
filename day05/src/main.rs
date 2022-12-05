2139f3617b96b08ff66e779ac77f65456bb3b4e7
Bartosz Marczyński <marczynski.bartosz@gmail.com>
2022-12-05 08:38:25 +0100

    day05: part 1

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,3 +1,108 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let filename: &str = &args[1];
+
+    let (stack, instructions) = read_input(filename);
+
+    let stack = to_stacks(stack);
+    let instructions = to_instructions(instructions);
+
+    let stack = exec(stack, instructions);
+
+    println!("{}", read_top(stack));
+}
+
+fn read_top(stacks: Vec<Vec<char>>) -> String {
+    let mut result: Vec<char> = Vec::new();
+
+    for stack in stacks {
+        if stack.len() > 0 {
+            result.push(stack[stack.len() - 1])
+        }
+    }
+
+    result.iter().collect()
+}
+
+fn exec(mut stacks: Vec<Vec<char>>, moves: Vec<Move>) -> Vec<Vec<char>> {
+    for m in moves {
+        for _ in 0..m.count {
+            let to_push = stacks[m.from].pop().unwrap();
+            stacks[m.to].push(to_push);
+        }
+    }
+
+    stacks
+}
+
+#[derive(Clone, Copy, Debug)]
+struct Move {
+    count: usize,
+    from: usize,
+    to: usize,
+}
+
+impl From<&str> for Move {
+    fn from(s: &str) -> Self {
+        let vec = s
+            .split_whitespace()
+            .filter(|sub| !sub.is_empty())
+            .filter_map(|sub| sub.parse::<usize>().ok())
+            .collect::<Vec<usize>>();
+        Self {
+            count: vec[0],
+            from: vec[1] - 1,
+            to: vec[2] - 1,
+        }
+    }
+}
+
+fn to_instructions(s: String) -> Vec<Move> {
+    s.split("\n")
+        .filter(|l| !l.is_empty())
+        .map(|l| Move::from(l))
+        .collect()
+}
+
+fn to_stacks(s: String) -> Vec<Vec<char>> {
+    let count = s
+        .split("\n")
+        .filter(|l| !l.is_empty() && l.contains("1"))
+        .next()
+        .unwrap()
+        .rsplit(" ")
+        .filter(|s| !s.is_empty())
+        .map(|n| n.parse::<usize>().unwrap())
+        .next()
+        .unwrap();
+
+    let mut stacks: Vec<Vec<char>> = vec![Vec::new(); count];
+
+    for row in s.split("\n").filter(|l| !l.is_empty()) {
+        for (index, chunk) in row.chars().collect::<Vec<char>>().chunks(4).enumerate() {
+            if chunk.contains(&'[') {
+                stacks[index].push(chunk[1]);
+            }
+        }
+    }
+
+    for stack in &mut stacks {
+        stack.reverse();
+    }
+
+    stacks
+}
+
+fn read_input(filename: &str) -> (String, String) {
+    let contents = fs::read_to_string(filename).expect(&format!(
+        "Something went wrong reading the file: {}",
+        filename
+    ));
+    let mut iter = contents
+        .split("\n\n")
+        .filter(|l| !l.is_empty())
+        .map(|s| s.to_string());
+    (iter.next().unwrap(), iter.next().unwrap())
 }
